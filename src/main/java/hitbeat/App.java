aab489c6659092baded81ce28045a2431b9843ba
Bruno Martins <bgmartins@ucs.br>
2023-09-10 20:25:23 -0300

    feat: :tada: estrutura inicial com bibliotecas front-end
    Primeiro versionamento do projeto com a estrutura inicial para programar organizada com o maven (gerenciamento de libs do java).

---
+++ src/main/java/hitbeat/App.java
@@ -0,0 +1,18 @@
+package hitbeat;
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    @Override     
+    public void start(Stage primaryStage) throws Exception {      
+    }    
+}
