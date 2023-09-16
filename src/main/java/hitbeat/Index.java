f2be622a359836c9cd3484b5e312d67b7a7c93bd
Bruno Martins <bgmartins@ucs.br>
2023-09-15 21:31:29 -0300

    feat: :art: Tentando usar media player do javafx

--- src/main/java/hitbeat/Index.java
+++ src/main/java/hitbeat/Index.java
@@ -3,7 +3,11 @@ package hitbeat;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
 public class Index extends Application{
     
@@ -18,6 +22,14 @@ public class Index extends Application{
         primaryStage.setTitle("HitBeat");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        String source = getClass().getResource("media/HitBeat.mp4").toString();
+        Media media = new Media(source);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+
+        MediaView mediaView = new MediaView(mediaPlayer);
+        ((Group) scene.getRoot()).getChildren().add(mediaView);
     }
 
     public static void main(String[] args) {
