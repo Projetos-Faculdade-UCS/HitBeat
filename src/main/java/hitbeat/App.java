2cb92d080014ad3ffc3b57a6bc9177d01585553b
Marco Aurélio da Rosa Haubrich <marhaubrich@ucs.br>
2023-09-19 13:59:55 -0300

    feat: :sparkles: Adicionado Hibernate

--- src/main/java/hitbeat/App.java
+++ src/main/java/hitbeat/App.java
@@ -1,5 +1,11 @@
 package hitbeat;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import hitbeat.model.Track;
 import hitbeat.view.IndexView;
 
 /**
@@ -8,6 +14,23 @@ import hitbeat.view.IndexView;
  */
 public class App {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = null;
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry =
+            new StandardServiceRegistryBuilder()
+                    .build();     
+    try {
+        sessionFactory =
+                new MetadataSources(registry)             
+                        .addAnnotatedClass(Track.class)   
+                        .buildMetadata()                  
+                        .buildSessionFactory();           
+    }
+    catch (Exception e) {
+        // The registry would be destroyed by the SessionFactory, but we
+        // had trouble building the SessionFactory so destroy it manually.
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
         IndexView.main(args);
     }
 }
--- src/main/java/hitbeat/model/Track.java
+++ src/main/java/hitbeat/model/Track.java
@@ -2,7 +2,18 @@ package hitbeat.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "track")
 public class Track {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String nome;
     private Date dt_criacao;
     private int duracao; // em segundos
