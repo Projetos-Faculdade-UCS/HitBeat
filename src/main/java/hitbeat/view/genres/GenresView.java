15b9994053e61c8d8b13f7ab17fced6a181191fd
Marco Aurélio da Rosa Haubrich <marhaubrich@ucs.br>
2023-10-04 15:08:59 -0300

    fix: :bug: Fix na UI e performance de Genres

--- src/main/java/hitbeat/view/genres/GenresView.java
+++ src/main/java/hitbeat/view/genres/GenresView.java
@@ -2,13 +2,11 @@ package hitbeat.view.genres;
 
 import hitbeat.controller.genres.GenresController;
 import hitbeat.model.Genre;
+import hitbeat.view.Layout;
+import hitbeat.view.base.widgets.Margin;
 import hitbeat.view.base.widgets.listview.ListView;
 import javafx.collections.ObservableList;
-import javafx.scene.Node;
 import javafx.scene.control.ListCell;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 
 public class GenresView extends ListView<Genre> {
     private ObservableList<Genre> genres;
@@ -20,33 +18,40 @@ public class GenresView extends ListView<Genre> {
         this.setItems(genres);
 
         this.setCellFactory(genre -> {
-            return new ListCell<Genre>() {
-                @Override
-                protected void updateItem(Genre genre, boolean empty) {
-                    super.updateItem(genre, empty);
-                    if (genre == null || empty) {
-                        setText(null);
-                        setGraphic(null);
-                        // hide the cell
-                        setId("hidden-list-cell");
-                        return;
-                    } else {
-                        VBox vbox = new VBox();
-                        Node genreCell = new GenreCell(genre);
-                        VBox.setVgrow(genreCell, Priority.ALWAYS);
-                        HBox.setHgrow(genreCell, Priority.ALWAYS);
-                        vbox.getChildren().add(genreCell);
-
-                        HBox.setHgrow(vbox, Priority.ALWAYS);
-
-                        setGraphic(vbox);
-                        setOnMouseClicked(event -> {
-                            System.out.println("Clicked on " + genre.getName());
-                        });
-                        setId("list-cell");
-                    }
-                }
-            };
+            return new ListGenreCell();
         });
+    }
+
+    class ListGenreCell extends ListCell<Genre> {
+        private GenreCell genreCell;
+
+        public ListGenreCell() {
+            super();
+            this.genreCell = new GenreCell(null);
+        }
+
+        @Override
+        protected void updateItem(Genre genre, boolean empty) {
+            super.updateItem(genre, empty);
+            if (genre == null || empty) {
+                genreCell.prefWidthProperty().unbind();
+                setText(null);
+                setGraphic(null);
+                // hide the cell
+                setId("hidden-list-cell");
+                return;
+            } else {
+                genreCell.setGenre(genre);
+                genreCell.prefWidthProperty().bind(Layout.getInstance().getContentWidth().subtract(20));
+
+                Margin margin = new Margin(genreCell, 0, 0, 8, 0);
+
+                setGraphic(margin);
+                setOnMouseClicked(event -> {
+                    System.out.println("Clicked on " + genre.getName());
+                });
+                genreCell.setId("list-cell");
+            }
+        }
     }
 }
