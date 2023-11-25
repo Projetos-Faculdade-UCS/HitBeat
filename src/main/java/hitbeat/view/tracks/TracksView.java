8044cbd8dba35e5f1c141b00f4d6dc1c904135f5
Marco Haubrich <marcolino.haubrich@gmail.com>
2023-11-24 21:37:46 -0300

    refactor: :recycle: Refactor TracksView to use controller.fetchAll() directly

--- src/main/java/hitbeat/view/tracks/TracksView.java
+++ src/main/java/hitbeat/view/tracks/TracksView.java
@@ -1,18 +1,14 @@
 package hitbeat.view.tracks;
 
 import hitbeat.controller.tracks.TracksController;
-import hitbeat.model.Track;
 import hitbeat.view.base.widgets.generic_list.GenericTrackList;
-import javafx.collections.ObservableList;
 
 public class TracksView extends GenericTrackList {
-    private ObservableList<Track> tracks;
     private final TracksController controller = new TracksController();
 
     public TracksView() {
         super();
-        tracks = controller.fetchAll();
-        this.setTracks(tracks);
+        this.setTracks(controller.fetchAll());
     }
 
     @Override
