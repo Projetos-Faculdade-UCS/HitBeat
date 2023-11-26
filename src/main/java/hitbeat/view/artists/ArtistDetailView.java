e9a136534225925a4b6352026dfbb4b5e1b5e75e
Marco Haubrich <marcolino.haubrich@gmail.com>
2023-11-26 17:56:48 -0300

    feat: :sparkles: Add ArtistDetailView class to display artist details

---
+++ src/main/java/hitbeat/view/artists/ArtistDetailView.java
@@ -0,0 +1,14 @@
+package hitbeat.view.artists;
+
+import hitbeat.model.Artist;
+import hitbeat.view.base.widgets.generic_list.GenericTrackList;
+
+public class ArtistDetailView extends GenericTrackList {
+
+    public ArtistDetailView(Artist artist) {
+        super();
+
+        this.setTracks(artist.getTracks());
+    }
+    
+}
